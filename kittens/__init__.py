233a4b5dd1d8355f4c50c672378d297524fd19d8
Sam Lucidi <sam@samlucidi.com>
2014-05-14 21:28:56 -0400

    Fix ability to run from package

--- kittens/__init__.py
+++ kittens/__init__.py
@@ -90,25 +90,3 @@ def fire_kitten(phone_number, message, image_url):
 		"message": "The warhead has been armed."
 	}
 	return requests.get(mogreet_url, params=params).json()
-
-def print_usage():
-	"""
-	Print a usage message and then quit.
-
-	"""
-
-	print("Usage: %s phone_number message [repetitions]" % sys.argv[0])
-	sys.exit(1)
-
-if __name__ == '__main__':
-	if len(sys.argv) < 3:
-		print_usage()
-
-	phone_number = sys.argv[1]
-	message = sys.argv[2]
-	if len(sys.argv) > 3:
-		repetitions = sys.argv[3]
-	else:
-		repetitions = 1
-	for i in range(repetitions):
-		print(fire_kitten(phone_number, message, _select_image()))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kittens',
-    version='0.1.0',
+    version='0.1.2',
     author='Samuel "mansam" Lucidi',
     author_email="mansam@csh.rit.edu",
     packages=['kittens'],
